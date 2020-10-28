b4a145c345a7250b9a7c50a016842850d958e5f1
Dimple Arora <info@raspbians.com>
2020-10-28 18:32:49 +0530

    Init Commit

---
+++ js/nav.js
@@ -0,0 +1,20 @@
+document.querySelectorAll('#nav').forEach((e) => {
+    e.innerHTML = `<div class="scrollmenu">
+    <a href="../index.html">Home</a>
+    <a href="exoplanets.html">Exoplanets</a>
+    <a href="hubblespacetelescope.html">Hubble Space Telescope</a>
+    <a href="haleboppcomet.html">Hale Bopp Commet</a>
+    <a href="dwarfplanet.html">Dwarf Planet</a>
+    <a href="constellation.html">Consetellation</a>
+    <a href="galaxy.html">Galaxy</a>  
+    <a href="#base">Base</a>
+    <a href="#custom">Custom</a>
+    <a href="#more">More</a>
+    <a href="#logo">Logo</a>
+    <a href="#friends">Friends</a>
+    <a href="#partners">Partners</a>
+    <a href="#people">People</a>
+    <a href="#work">Work</a>
+    </div>
+    `
+})
