8dd6c4b49bedaf6cf21184a6a77dfb2a2a305484
Dimple Arora <info@raspbians.com>
2020-10-28 19:43:47 +0530

    added NavIC statellite

--- js/nav.js
+++ js/nav.js
@@ -7,14 +7,12 @@ document.querySelectorAll('#nav').forEach((e) => {
     <a href="dwarfplanet.html">Dwarf Planet</a>
     <a href="constellation.html">Consetellation</a>
     <a href="galaxy.html">Galaxy</a>  
-    <a href="#base">Base</a>
-    <a href="#custom">Custom</a>
-    <a href="#more">More</a>
-    <a href="#logo">Logo</a>
-    <a href="#friends">Friends</a>
-    <a href="#partners">Partners</a>
-    <a href="#people">People</a>
-    <a href="#work">Work</a>
+    <a href="#base">ISS</a>
+    <a href="#custom">Rover</a>
+    <a href="#more">Asteroid Belt</a>
+    <a href="NAVIC-satellite"> NavIC Satellite</a>
+    <a href="#friends">Quiz</a>
+    
     </div>
     `
 })
