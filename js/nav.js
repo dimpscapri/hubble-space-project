a82f58573f0b68f59a4367897a0dea523c286ab4
Dimple Arora <info@raspbians.com>
2020-10-28 21:52:14 +0530

    ISS n Rover added

--- js/nav.js
+++ js/nav.js
@@ -1,16 +1,16 @@
 document.querySelectorAll('#nav').forEach((e) => {
     e.innerHTML = `<div class="scrollmenu">
     <a href="../index.html">Home</a>
-    <a href="exoplanets.html">Exoplanets</a>
-    <a href="hubblespacetelescope.html">Hubble Space Telescope</a>
-    <a href="haleboppcomet.html">Hale Bopp Commet</a>
-    <a href="dwarfplanet.html">Dwarf Planet</a>
-    <a href="constellation.html">Consetellation</a>
-    <a href="galaxy.html">Galaxy</a>  
-    <a href="iss.html">ISS</a>
-    <a href="#custom">Rover</a>
+    <a href="./exoplanets.html">Exoplanets</a>
+    <a href="./hubblespacetelescope.html">Hubble Space Telescope</a>
+    <a href="./haleboppcomet.html">Hale Bopp Commet</a>
+    <a href="./dwarfplanet.html">Dwarf Planet</a>
+    <a href="./constellation.html">Consetellation</a>
+    <a href="./galaxy.html">Galaxy</a>  
+    <a href="./iss.html">ISS</a>
+    <a href="./rover.html">Rover</a>
     <a href="#more">Asteroid Belt</a>
-    <a href="NAVIC-satellite.html"> NavIC Satellite</a>
+    <a href="./NAVIC-satellite.html"> NavIC Satellite</a>
     <a href="#friends">Quiz</a>
     
     </div>
