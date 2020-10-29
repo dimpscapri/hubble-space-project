4abf0ee40c3b94b16d8719b5a550319bf557486a
Dimple Arora <info@raspbians.com>
2020-10-29 21:47:34 +0530

    Exoplanets

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,5 @@
 document.querySelectorAll('#nav').forEach((e) => {
-    e.innerHTML = `<div class="scrollmenu">
+    e.innerHTML = `<div class="topnav">
     <a href="../index.html">Home</a>
     <a href="./exoplanets.html">Exoplanets</a>
     <a href="./hubblespacetelescope.html">Hubble Space Telescope</a>
@@ -11,6 +11,23 @@ document.querySelectorAll('#nav').forEach((e) => {
     <a href="./rover.html">Rover</a>
     <a href="./asteroidbelt.html">Asteroid Belt</a>
     <a href="./NAVIC-satellite.html">NavIC</a>
+    <a href="./quiz.html">Quiz</a>
+    <a href="./facts.html">Facts</a>
+
+
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
     </div>
     `
 })
+
+
+function myFunction() {
+  var x = document.getElementById("nav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
